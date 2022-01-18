1e37439cd147467fd1c83770822b6b96f3a5329a
pVICTORIAAAA <97870251+pVICTORIAAAA@users.noreply.github.com>
2022-01-18 12:41:42 +0100

    // secendario logino
    // second login

---
+++ secondlogin.lua
@@ -0,0 +1,14 @@
+local username_true = {}
+local username = globals.get_username()
+username_true['Victoriaaaa'] = true
+
+
+local function sendtrue()
+    if username_true[username] == true then
+      return true
+    else
+      return false
+    end
+end
+
+return sendtrue()
